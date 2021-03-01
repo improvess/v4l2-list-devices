244b341e8e6b40c57ecf9eb5dea47f217ca28c45
doleron <doleron@gmail.com>
2021-03-01 00:29:02 -0300

    Add files via upload

---
+++ include/list_devices.hpp
@@ -0,0 +1,147 @@
+/* ---------------------------------------------------------------------
+ * Luiz Carlos doleron @ 2021
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ----------------------------------------------------------------------
+ */
+
+
+#ifndef LIST_DEVICES_H_
+#define LIST_DEVICES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+#include <linux/types.h>
+#include <linux/v4l2-common.h>
+#include <linux/v4l2-controls.h>
+#include <linux/videodev2.h>
+#include <linux/media.h>
+#include <sys/ioctl.h>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+namespace v4l2
+{
+    namespace devices
+    {
+
+        struct DEVICE_INFO
+        {
+            std::string device_description;
+            std::string bus_info;
+            std::vector<std::string> device_paths;
+        };
+
+        void list(std::vector<DEVICE_INFO> &devices)
+        {
+
+            std::vector<std::string> files;
+
+            const std::string dev_folder = "/dev/";
+
+            for (const auto &entry : std::filesystem::directory_iterator(dev_folder))
+            {
+                std::string file_path = entry.path().string();
+                if (std::filesystem::is_symlink(entry))
+                {
+                    continue;
+                }
+                const int fd = open(file_path.c_str(), O_RDWR);
+                v4l2_capability capability;
+                if (fd >= 0 && ioctl(fd, VIDIOC_QUERYCAP, &capability) >= 0)
+                {
+                    files.push_back(file_path);
+                }
+                close(fd);
+            }
+
+            std::sort(files.begin(), files.end());
+
+            struct v4l2_capability vcap;
+
+            std::map<std::string, DEVICE_INFO> device_map;
+
+            for (const auto &file : files)
+            {
+                int fd = open(file.c_str(), O_RDWR);
+                std::string bus_info;
+                std::string card;
+
+                if (fd < 0)
+                    continue;
+                int err = ioctl(fd, VIDIOC_QUERYCAP, &vcap);
+                if (err)
+                {
+                    struct media_device_info mdi;
+
+                    err = ioctl(fd, MEDIA_IOC_DEVICE_INFO, &mdi);
+                    if (!err)
+                    {
+                        if (mdi.bus_info[0])
+                            bus_info = mdi.bus_info;
+                        else
+                            bus_info = std::string("platform:") + mdi.driver;
+
+                        if (mdi.model[0])
+                            card = mdi.model;
+                        else
+                            card = mdi.driver;
+                    }
+                }
+                else
+                {
+                    bus_info = reinterpret_cast<const char *>(vcap.bus_info);
+                    card = reinterpret_cast<const char *>(vcap.card);
+                }
+                close(fd);
+
+                if (!bus_info.empty() && !card.empty())
+                {
+
+                    if (device_map.find(bus_info) != device_map.end())
+                    {
+                        DEVICE_INFO &device = device_map.at(bus_info);
+                        device.device_paths.emplace_back(file);
+                    }
+                    else
+                    {
+                        DEVICE_INFO device;
+                        device.device_paths.emplace_back(file);
+                        device.bus_info = bus_info;
+                        device.device_description = card;
+                        device_map.insert(std::pair<std::string, DEVICE_INFO>(bus_info, device));
+                    }
+                }
+            }
+
+            for (const auto &row : device_map) {
+                devices.emplace_back(row.second);
+            }
+        }
+    }
+
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "list_devices.hpp"
+
+int main() {
+
+    std::vector<v4l2::devices::DEVICE_INFO> devices;
+
+    v4l2::devices::list(devices);
+
+    for (const auto & device : devices) {
+        std::cout << device.device_description <<  " at " << device.bus_info << " is attached to\n";
+
+        for (const auto & path : device.device_paths) {
+            std::cout << path << "\n";
+        }
+    }
+
+}
