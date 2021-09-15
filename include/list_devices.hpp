01875bd398b64d7c188aa87c339acf7c5e92b05a
doleron <doleron@gmail.com>
2021-09-15 12:48:45 -0300

    added resolve_path function

--- include/list_devices.hpp
+++ include/list_devices.hpp
@@ -142,6 +142,73 @@ namespace v4l2
                 devices.emplace_back(row.second);
             }
         }
+
+        inline DEVICE_INFO resolve_path(const std::string & target_usb_bus_id)
+        {
+            DEVICE_INFO result;
+            result.bus_info = target_usb_bus_id;
+
+            std::vector<std::string> files;
+            std::vector<std::string> paths;
+
+            const std::string dev_folder = "/dev/";
+
+            DIR *dir;
+            struct dirent *ent;
+            if ((dir = opendir(dev_folder.c_str())) != NULL)
+            {
+                //duplicate entries for the same path requires full loop
+                while ((ent = readdir(dir)) != NULL)
+                {
+                    std::string file = dev_folder + ent->d_name;
+
+                    const int fd = open(file.c_str(), O_RDWR);
+                    v4l2_capability capability;
+                    if (fd >= 0)
+                    {
+                        int err = ioctl(fd, VIDIOC_QUERYCAP, &capability);
+                        if (err >= 0)
+                        {
+                            std::string bus_info;
+                            struct media_device_info mdi;
+
+                            err = ioctl(fd, MEDIA_IOC_DEVICE_INFO, &mdi);
+                            if (!err)
+                            {
+                                if (mdi.bus_info[0])
+                                    bus_info = mdi.bus_info;
+                                else
+                                    bus_info = std::string("platform:") + mdi.driver;
+                            }
+                            else
+                            {
+                                bus_info = reinterpret_cast<const char *>(capability.bus_info);
+                            }
+
+                            if (!bus_info.empty())
+                            {   
+                                if (bus_info.compare(target_usb_bus_id) == 0) {
+
+                                    paths.push_back(file);
+                                }
+                            }
+                        }
+                    }
+                    close(fd);
+                }
+                if (!paths.empty()) {
+                    std::sort(paths.begin(), paths.end());
+                    result.device_paths = paths;
+                }
+                closedir(dir);
+            }
+            else
+            {
+                std::string msg = "Cannot list " + dev_folder + " contents!";
+                throw std::runtime_error(msg);
+            }
+            return result;
+        }
     }
 
 }
